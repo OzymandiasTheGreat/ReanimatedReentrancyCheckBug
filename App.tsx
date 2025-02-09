7237d156979cf25e5ffb96d5f3fb34d87c94aa92
Tomas Ravinskas <tomas@tomasrav.me>
2025-02-09 16:19:36 +0200

    Initial commit (MRE)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
+import React, { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { runOnRuntime } from 'react-native-reanimated';
+import { getWorkletRuntime, MarkdownTextInput, parseExpensiMark } from '@expensify/react-native-live-markdown';
 
 export default function App() {
+  const worklet = useCallback(() => {
+    "worklet"
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.inputContainer}>
+        <MarkdownTextInput
+          editable
+          multiline
+          parser={parseExpensiMark}
+          style={styles.input}
+          onChangeText={() => runOnRuntime(getWorkletRuntime(), worklet)()}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  input: {
+    flex: 1,
+  },
+  inputContainer: {
+    borderColor: "#555",
+    borderRadius: 8,
+    borderWidth: 1,
+    height: 96,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    width: "80%",
+  },
+})
